c7374e8894f9fe9873dced17934aab917ce33bc8
unknown <Bezy427>
2026-07-26 02:40:33 +0200

    Second Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,19 @@
+
 function App() {
-  return <h1>Hello World of  React!</h1>
+  const hours = new Date().getHours()
+  let timeOfDay
+
+  if (hours < 12) {
+    timeOfDay = "morning"
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = "afternoon"
+  } else if (hours < 21) {
+    timeOfDay = "evening"
+  } else {
+    timeOfDay = "night"
+  }
+
+  return (
+    <h1>Good {timeOfDay}</h1>)
 }
 export default App;
