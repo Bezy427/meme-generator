2d93dd1769b627337c9242d2221c51cd4c61d5cd
unknown <Bezy427>
2026-07-27 00:21:44 +0200

    Deleting the contact.jsx and header.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
-import Contact from "./Contact";
+import Joke from "./Joke"
 
-function App(props) {
-  
-
+export default function App() {
   return (
-    <div className="contacts">
-      <Contact 
-        img="../images/mr-whiskerson.png"
-        name="Mr. Whiskerson"
-        phone="(212) 555 1234"
-        email="mr.whickerson@catnap.com"
+    <main>
+      <Joke 
+        punchline="I can't wait to see her face light up when she opens it."
+        upvotes={10}
+        isPun={true}
       />
-      <Contact />
-      <Contact />
-      <Contact />
-    </div>
+      <Joke
+        setup="How did the hacker escape the police?"
+        punchline="He just ransoware!"
+      />
+      <Joke 
+        setup="Why don't pirates travel on mountain roads?"
+        punchline="Scurvy."
+      />
+      <Joke 
+        setup="Why do bees stay in the hive in the winter?"
+        punchline="Swarm"
+      />
+      <Joke 
+        setup="What's the best thing about Switzerland?"
+        punchline="I don't know, but the flag is abig plus!"
+      />
+    </main>
   )
-}
-export default App;
+} 
