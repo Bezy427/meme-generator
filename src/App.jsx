c72d7ae1f2b9d6ed6a05cb4200bfecd53237e5a8
unknown <Bezy427>
2026-07-31 03:30:39 +0200

    Fourth Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
-import Joke from "./Joke"
+import {useState} from "react";
+import Header from "./Header";
+import Main from "./Main"
 
 export default function App() {
+  const [unreadMessages, setUnreadMessages] = useState(["a"])
+ 
   return (
-    <main>
-      <Joke 
-        punchline="I can't wait to see her face light up when she opens it."
-        upvotes={10}
-        isPun={true}
-      />
-      <Joke
-        setup="How did the hacker escape the police?"
-        punchline="He just ransoware!"
-      />
-      <Joke 
-        setup="Why don't pirates travel on mountain roads?"
-        punchline="Scurvy."
-      />
-      <Joke 
-        setup="Why do bees stay in the hive in the winter?"
-        punchline="Swarm"
-      />
-      <Joke 
-        setup="What's the best thing about Switzerland?"
-        punchline="I don't know, but the flag is abig plus!"
-      />
-    </main>
+    <>
+        {/* <Header />
+        <Main /> */}
+        {
+          unreadMessages.length > 0 &&
+          <h1>You have {unreadMessages.length} unread messages!</h1>
+        }  
+    </>
   )
 } 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,37 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import {useState} from "react"
 
-const root = createRoot(
-  document.getElementById("root")
-)
-root.render(
-    <App />
-)
+export default function Main() {
+    const [ingredients, setIngredients] = useState([])
+
+    const ingredientsListItems = ingredients.map(i => (
+      <li key={i}>{i}</li>  
+    ))
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients => [
+            ...prevIngredients,
+            newIngredient
+            ]
+        )
+    }
+
+    return (
+        <main>
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
+                <input 
+                    type="text"
+                    placeholder="e.g. oregano"
+                    arial-label="Add ingredients" 
+                    name="ingredient"
+                    />
+                <button>Add Ingredient</button>   
+            </form>
+            <ul>
+                {ingredientsListItems}
+            </ul>
+        </main>
+    )
+}
