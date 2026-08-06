a3ccfcec730521454cb11c17a356cc9cc1fb30fd
unknown <Bezy427>
2026-08-06 04:59:50 +0200

    14th commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,9 @@ export default function App() {
 
   const [dice, setDice] = useState(generateAllNewDice)
 
+  const gameWon = dice.every(die => die.isHeld) &&
+       dice.every(die => die.value === dice[0].value) 
+
   function generateAllNewDice() {
     return new Array(10)
       .fill(0)
@@ -94,7 +97,9 @@ export default function App() {
       <div className="dice-container">
         {diceElements}
       </div>
-      <button className="roll-dice" onClick={rollDice}>Roll</button>
+      <button className="roll-dice" onClick={rollDice}>
+        {gameWon ? "New Game" : "Roll"}
+      </button>
     </main>
   )
 } 
