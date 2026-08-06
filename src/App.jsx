66d6a20e36d506bc667a17c871a66cb81e8e30e5
unknown <Bezy427>
2026-08-06 04:42:36 +0200

    13th commit

--- src/App.jsx
+++ src/App.jsx
@@ -40,23 +40,26 @@ export default function App() {
   }
 
   function rollDice() {
-    setDice(generateAllNewDice)
+    setDice(oldDice => oldDice.map(die => 
+      die.isHeld ?
+        die :
+        { ...die, value: Math.ceil(Math.random() * 6)}
+    ))
   }
 
   function hold(id) {
     setDice(oldDice => oldDice.map(die => 
         die.id === id ?
-          {...die, isHeld: !die.isHeld} :
+          { ...die, isHeld: !die.isHeld} :
           die
-    )
-    )
+    ))
   }
 
   const diceElements = dice.map(dieObject => (
       <Die 
         key={dieObject.id} 
         value={dieObject.value} 
-        isHeld={dieObject}
+        isHeld={dieObject.isHeld}
         hold={() => hold(dieObject.id)}
       />
     )
@@ -85,6 +88,9 @@ export default function App() {
     //   {show && <WindowTracker />}
     // </main>
     <main>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze
+        it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
