aac2a5388982dabb2c6b8eb82b5aba26d5e5ea43
unknown <Bezy427>
2026-08-12 17:09:05 +0200

    creation of the header

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,16 @@
 import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")).render(
-  <>
-    <img src="react-logo.png" alt="react-logo" />
-    <h1>This is another element!</h1>
-  </>
+  <Page />
+)
+
+function Page() {
+  return (
+    <main>
+      <ol>
+        <li>React is a popular library, so i will be able to fit in with all the coolest devs out there!</li>
+        <li>I am more likely to get a job as a front end developer if i know React</li>
+      </ol>
+    </main>
   )
+}
