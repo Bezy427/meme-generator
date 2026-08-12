2bf2f0ac5226df0af8db12e539dd47f9535327d5
unknown <Bezy427>
2026-08-12 03:21:02 +0200

    Initial commit

--- src/Count.jsx
+++
@@ -1,6 +0,0 @@
-export default function Count(props) {
-    console.log("Count rendered!")
-    return (
-        <h2 className="count">{props.number}</h2>
-    )
-}
--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+createRoot(document.getElementById("root")).render(
+  <>
+    <h1>This is the list of why learning react is important!</h1>
+    <ol>
+      <li>It seems interesting!</li>
+      <li>More job oportunities</li>
+      <li>Easy to grasp</li>
+      <li>It has a great ecosystem!</li>
+    </ol>
+  </>
 )
