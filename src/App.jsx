84c64d0ca4e5f040d558228d60ab9fc1651caa0e
Bezy427 <rusamubezel@gmail.com>
2026-08-17 03:40:54 +0200

    creating states

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import Entry from "./components/Entry";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 export default function App() {
+  const [count, setCount] = useState(0)
+
+  function add() {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  function minus() {
+    setCount(prevCount => prevCount - 1)
+  }
+
   return (
-    <>
-      <Header />
-      <main className="container">
-        <Entry />
-      </main>
-    </>
+    <main>
+      <div>
+        <button onClick={minus} className="minus">-</button>
+        <h2>{count}</h2>
+        <button onClick={add} className="minus">+</button>
+      </div>
+    </main>
+    // <>
+    //   <Header />
+    //   <main className="container">
+    //     <Entry />
+    //   </main>
+    // </>
   )
 }
